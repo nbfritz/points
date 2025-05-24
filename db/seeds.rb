0e9c971cb324fa40ba312f2489514607f77a82a8
Nathan Fritz <nbfritz@gmail.com>
2025-05-24 13:40:41 -0500

    Add stock authentication system with some tweaks. (#13)
    Adds the default rails generated authentication system to the app, with
    some tweaks:
    * no attempt to send emails in development
    * adds a session#show page to act as a temporary home page (and indicate
    successful login)
    * adds the option to logout on that page
    * renames email_address to email on the users table
    * limits the resource definition to just the utilized actions for
    session

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+seed_email, seed_password = ENV.values_at("SEED_EMAIL", "SEED_PASSWORD")
+
+if seed_email && seed_password
+  Rails.logger.info "Seeding Admin User"
+  User.create(
+    first_name: "Admin",
+    last_name: "User",
+    email: seed_email,
+    password_digest: BCrypt::Password.create(seed_password)
+  )
+else
+  Rails.logger.info "Skipping Admin User (SEED_EMAIL and SEED_PASSWORD not set)"
+end
